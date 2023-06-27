17013a8ee3efb0cbbbf1a226b1be814584d2ccf5
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2023-06-27 14:03:28 +0300

    initialize BettingData.BLL project and XmlReaderService.cs class

---
+++ src/BettingData/BettingData.BLL/Services/XmlReaderService.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Xml;
+
+namespace BettingData.BLL.Services
+{
+    public class XmlReaderService
+    {
+        private readonly String _URLString = "https://sports.ultraplay.net/sportsxml?clientKey=9C5E796D-4D54-42FD-A535-D7E77906541A&sportId=2357&days=7";
+        private readonly XmlTextReader _reader;
+
+        public XmlReaderService()
+        {
+            _reader = new XmlTextReader(_URLString);
+        }
+
+        public void ReadFile()
+        {
+            while (_reader.Read())
+            {
+                // Do some work here on the data.
+                Console.WriteLine(_reader.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
